462f732370e5c46e776c77ddda7320616a47164d
JskAct <jishukai@act.buaa.edu.cn>
2018-03-26 13:54:05 +0800

    deal with all figures UN's metric report csv. output xxxDecode.csv including field name. the first field is prediction (stars/fileN*(1+Ca)).

--- decoder.py
+++ decoder.py
@@ -1,34 +1,61 @@
 import csv
 import sys
 import os
+import math
 from functools import reduce
 
 class myFile(object):
     # to collect file-level info from class-level reports
-    argsKind = ['Name', 'AvgCyclomatic', 'AvgLine', 'AvgLineCode', 'CountClassBase', 'CountClassCoupled', 'CountClassDerived', 'CountDeclClass', 'CountDeclClassMethod', 'CountDeclClassVariable', 'CountDeclFunction', 'CountDeclInstanceMethod', 'CountDeclInstanceVariable', 'CountDeclMethod', 'CountDeclMethodAll', 'CountDeclMethodPrivate', 'CountDeclMethodProtected', 'CountDeclMethodPublic', 'CountStmt', 'CountStmtDecl', 'CountStmtExe', 'MaxCyclomatic', 'MaxInheritanceTree', 'MaxNesting', 'PercentLackOfCohesion', 'RatioCommentToCode', 'SumCyclomatic']
-    def __init__(self, args):
-        # args = [
-        # [0:3]   'Name', 'AvgCyclomatic', 'AvgLine', 'AvgLineCode',
-        # [4-10]  'CountClassBase', 'CountClassCoupled', 'CountClassDerived', 'CountDeclClass', 'CountDeclClassMethod', 'CountDeclClassVariable', 'CountDeclFunction',
-        # [11:17] 'CountDeclInstanceMethod', 'CountDeclInstanceVariable', 'CountDeclMethod', 'CountDeclMethodAll', 'CountDeclMethodPrivate', 'CountDeclMethodProtected', 'CountDeclMethodPublic',
-        # [18:20] 'CountStmt', 'CountStmtDecl', 'CountStmtExe',
-        # [21:26] 'MaxCyclomatic', 'MaxInheritanceTree', 'MaxNesting', 'PercentLackOfCohesion', 'RatioCommentToCode', 'SumCyclomatic']
+    argsField = ['Name', 'AvgCyclomatic', 'AvgCyclomaticModified', 'AvgCyclomaticStrict', 'AvgEssential', 'AvgLine', 'AvgLineBlank', 'AvgLineCode', 'AvgLineComment', 'CountClassBase', 'CountClassCoupled', 'CountClassDerived', 'CountDeclClass', 'CountDeclClassMethod', 'CountDeclClassVariable', 'CountDeclFile', 'CountDeclFunction', 'CountDeclInstanceMethod', 'CountDeclInstanceVariable', 'CountDeclMethod', 'CountDeclMethodAll', 'CountDeclMethodDefault', 'CountDeclMethodPrivate', 'CountDeclMethodProtected', 'CountDeclMethodPublic', 'CountInput', 'CountLine', 'CountLineBlank', 'CountLineCode', 'CountLineCodeDecl', 'CountLineCodeExe', 'CountLineComment', 'CountOutput', 'CountPath', 'CountSemicolon', 'CountStmt', 'CountStmtDecl', 'CountStmtExe', 'Cyclomatic', 'CyclomaticModified', 'CyclomaticStrict', 'Essential', 'MaxCyclomatic', 'MaxCyclomaticModified', 'MaxCyclomaticStrict', 'MaxEssential', 'MaxInheritanceTree', 'MaxNesting', 'PercentLackOfCohesion', 'RatioCommentToCode', 'SumCyclomatic', 'SumCyclomaticModified', 'SumCyclomaticStrict', 'SumEssential']
+    fileField = ['prediction','AvgCyclomatic', 'AvgCyclomaticModified', 'AvgCyclomaticStrict', 'AvgEssential', 'AvgLine', 'AvgLineBlank', 'AvgLineCode', 'AvgLineComment', 'AvgClassBase', 'AvgClassCoupled', 'AvgClassDerived', 'CountDeclClass', 'CountDeclClassMethod', 'CountDeclClassVariable', 'Ca', 'CountDeclFunction', 'CountDeclInstanceMethod', 'CountDeclInstanceVariable', 'CountDeclMethod', 'AvgDeclMethodAll', 'CountDeclMethodDefault', 'CountDeclMethodPrivate', 'CountDeclMethodProtected', 'CountDeclMethodPublic', 'CountLine', 'CountLineBlank', 'CountLineCode', 'CountLineCodeDecl', 'CountLineCodeExe', 'CountLineComment', 'CountSemicolon', 'CountStmt', 'CountStmtDecl', 'CountStmtExe', 'MaxCyclomatic', 'MaxCyclomaticModified', 'MaxCyclomaticStrict', 'MaxEssential', 'MaxInheritanceTree', 'MaxNesting', 'AvgPercentLackOfCohesion', 'RatioCommentToCode', 'SumCyclomatic', 'SumCyclomaticModified', 'SumCyclomaticStrict', 'SumEssential']
+    def __init__(self, args, ProjStars):
+        # args = [ [0]  'Name'
+        # [1:5]   'AvgCyclomatic', 'AvgCyclomaticModified', 'AvgCyclomaticStrict', 'AvgEssential'
+        # [5:9]   'AvgLine', 'AvgLineBlank', 'AvgLineCode', 'AvgLineComment'
+        ##[9:12]  'CountClassBase', 'CountClassCoupled', 'CountClassDerived' ##### CLASS #####
+        # [12:15] 'CountDeclClass', 'CountDeclClassMethod', 'CountDeclClassVariable' [15] 'CountDeclFile' NULL
+        # [16:20] 'CountDeclFunction', 'CountDeclInstanceMethod', 'CountDeclInstanceVariable', 'CountDeclMethod'
+        ##[20]    'CountDeclMethodAll' [25,32,33] 'CountInput', 'CountOutput', 'CountPath' NULL
+        # [21:25] 'CountDeclMethodDefault', 'CountDeclMethodPrivate', 'CountDeclMethodProtected', 'CountDeclMethodPublic'
+        # [26:32] 'CountLine', 'CountLineBlank', 'CountLineCode', 'CountLineCodeDecl', 'CountLineCodeExe', 'CountLineComment'
+        # [34:38] 'CountSemicolon', 'CountStmt', 'CountStmtDecl', 'CountStmtExe'
+        ##[38:42] 'Cyclomatic', 'CyclomaticModified', 'CyclomaticStrict', 'Essential' ##### CLASS #####
+        # [42:46] 'MaxCyclomatic', 'MaxCyclomaticModified', 'MaxCyclomaticStrict', 'MaxEssential'
+        ##[46,48] 'MaxInheritanceTree','PercentLackOfCohesion' ##### CLASS #####
+        # [47]    'MaxNesting',
+        # [49:54] 'RatioCommentToCode', 'SumCyclomatic', 'SumCyclomaticModified', 'SumCyclomaticStrict', 'SumEssential']
+        # self.args get from file-level reports, so some item maybe 0 or null
         self.args = args
-        # self.args is file-level reports, so some item maybe 0 or null
         # the following metrics will add up from 0 and finally divid self.CountClass to get self.avgsth.
         self.CountClassBase = 0 # AvgClassBase
         self.CountClassCoupled = 0
         self.CountClassDerived = 0
         self.CountDeclMethodAll = 0
-        self.MaxInheritanceTree = 0
-        # this metrics remain what it means, that is, not do avg math
         self.CountPercentLackOfCohesion = 0
-        self.CountClass = 0
+        self.AvgClassBase = 0
+        self.AvgClassCoupled = 0
+        self.AvgClassDerived = 0
+        self.AvgDeclMethodAll = 0
+        self.AvgPercentLackOfCohesion = 0
+        # this metrics remain what it means, that is, not do avg math
+        self.MaxInheritanceTree = 0
         # self.CountClass is used for get self.avgsth, and will not output to file.
+        self.CountClass = 0
+        # self.Ca and self.ProjStars is used to calculate prediction
         self.Ca = 0
+        self.ProjStars = int(ProjStars)
+        # set by self.setprediction()
+        self.prediction = 0
+
+    def setprediction(self, pre):
+        self.prediction = pre
 
     def output(self):
-        return reduce(lambda str1,str2: str1+','+str2, map(str, self.args[1:4]+self.args[7:14]+self.args[15:22]+self.args[23:24]+self.args[25:27]))
+        self.res = [self.prediction]+self.args[1:9]+[self.AvgClassBase, self.AvgClassCoupled, self.AvgClassDerived]
+        self.res += self.args[12:15] +[self.Ca]+ self.args[16:20]+[self.AvgDeclMethodAll]
+        self.res += self.args[21:25]+self.args[26:32]+self.args[34:38]+self.args[42:46]+[self.MaxInheritanceTree]
+        self.res += [self.args[47],self.AvgPercentLackOfCohesion]+self.args[49:]
+        return reduce(lambda str1,str2: str1+','+str2, map(str, self.res))
 
 class myClass(object):
     # to collect class-level info from Understand reports and restore the full_name of the file which it belong to
@@ -36,25 +63,31 @@ class myClass(object):
         self.belongToFile = fileName
         #self.args = args
 
-    def setArgs(self,args):
-        self.args = args
-
-if len(sys.argv) != 4:
+if len(sys.argv) != 5:
     print(len(sys.argv))
-    print( 'wrong call. usage: python decoder.py <projName> <AnalysisDir> <codeDir>')
+    print( 'wrong call. usage: python decoder.py <projName> <AnalysisDir> <codeDir> <starsNum>')
     # python decoder.py butterknifeAnalysis D:\科研\CodeQualityAnalysis\CodeAnalysis\JavaSampling\butterknifeAnalysis\ D:\CodeRepertory\Java\butterknife
     exit()
-_, projName, projDir, codeDir = sys.argv
+_, projName, projDir, codeDir, starsNum = sys.argv
 csvFilePath = os.path.join(projDir, projName+".csv")
-resultFilePath = os.path.join(projDir, projName+'Decode.txt')
+resultFilePath = os.path.join(projDir, projName+'Decode.csv')
 fileClassFilePath = os.path.join(projDir, projName+'_text', 'Files.txt')
 importFilePath = os.path.join(projDir, projName+'_text', 'Imports.txt')
+projMetricsFilePath = os.path.join(projDir, projName+'_text', 'ProjectMetrics.txt')
 classDict = {}
 fileDict = {}
 fileName = ""
+projFileNum = 0
 
-with open(fileClassFilePath, 'r') as fileClassFile:
-    # build up classDict which contain all the myClass object
+# get projFileNum from projectMetrics report file
+with open(projMetricsFilePath, 'r') as projFile:
+    for line in projFile.readlines():
+        if line.find('Files:') == 0:
+            projFileNum = int(line.split(':')[1].strip())
+            break
+
+# build up classDict which contain all the myClass object
+with open(fileClassFilePath, 'r', encoding='UTF-8') as fileClassFile:
     line = fileClassFile.readline()
     while line:
         if line.find(codeDir) == 0:
@@ -74,6 +107,10 @@ with open(fileClassFilePath, 'r') as fileClassFile:
 
 
 with open(resultFilePath, 'w') as resFile:
+    # get fieldnames for csv
+    fieldnames = myFile.fileField
+    writer = csv.DictWriter(resFile, fieldnames=fieldnames)
+    writer.writeheader()
     with open(csvFilePath, 'r') as csvFile:
         # build up fileDict which contains all myFile object to collect file-level metrics
         readCSV = csv.reader(csvFile, delimiter=',')
@@ -81,7 +118,7 @@ with open(resultFilePath, 'w') as resFile:
             if row[0] == 'File':
                 # file record
                 fileName = row[1]
-                fileDict[fileName] = myFile(row[1:])
+                fileDict[fileName] = myFile(row[1:], starsNum)
             elif row[0].find("Class") != -1 or row[0].find("Interface") != -1:
                 # class record
                 className = row[1]
@@ -89,14 +126,15 @@ with open(resultFilePath, 'w') as resFile:
                     fileName = classDict[className].belongToFile
                     if fileName in fileDict:
                         fileDict[fileName].CountClass += 1
-                        fileDict[fileName].CountClassBase += int(row[5])
-                        fileDict[fileName].CountClassCoupled += int(row[6])
-                        fileDict[fileName].CountClassDerived += int(row[7])
-                        fileDict[fileName].CountDeclMethodAll += int(row[15])
-                        fileDict[fileName].MaxInheritanceTree = max(fileDict[fileName].MaxInheritanceTree, int(row[23]))
-                        fileDict[fileName].CountPercentLackOfCohesion += int(row[25])
+                        fileDict[fileName].CountClassBase += int(row[10])
+                        fileDict[fileName].CountClassCoupled += int(row[11])
+                        fileDict[fileName].CountClassDerived += int(row[12])
+                        fileDict[fileName].CountDeclMethodAll += int(row[21])
+                        fileDict[fileName].MaxInheritanceTree = max(fileDict[fileName].MaxInheritanceTree, int(row[47]))
+                        fileDict[fileName].CountPercentLackOfCohesion += int(row[49])
 
     with open(importFilePath, 'r') as imFile:
+        # to calculate file.Ca
         fileName = ''
         readIm = imFile.readline()
         while readIm:
@@ -135,7 +173,8 @@ with open(resultFilePath, 'w') as resFile:
             file.AvgDeclMethodAll = 0
             file.AvgPercentLackOfCohesion = 100
 
-        resFile.write(file.output() +','+str(file.AvgClassBase) +','+str(file.AvgClassCoupled) +','+str(file.AvgClassDerived) +','+str(file.AvgDeclMethodAll) +','+str(file.MaxInheritanceTree) +','+str(file.AvgPercentLackOfCohesion) +','+str(file.Ca) +',' +'\n')
+        file.setprediction(math.log(file.ProjStars/projFileNum * (1 + file.Ca)))
+        resFile.write(file.output()+'\n')
         # for debug
         if file.Ca != 0:
             print('Ca: '+ str(file.Ca) + '\t| ' + file.args[0])
